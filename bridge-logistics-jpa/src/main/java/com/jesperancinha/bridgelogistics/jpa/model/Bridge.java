9fa414e93085a27933c6616204ae4cc4e333bfb3
Joao Esperancinha <jofisaes@gmail.com>
2020-03-21 17:32:52 +0100

    First record class

--- bridge-logistics-jpa/src/main/java/com/jesperancinha/bridgelogistics/jpa/model/Bridge.java
+++ bridge-logistics-jpa/src/main/java/com/jesperancinha/bridgelogistics/jpa/model/Bridge.java
@@ -1,35 +1,34 @@
 package com.jesperancinha.bridgelogistics.jpa.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "BRIDGE")
 @NamedQueries({
         @NamedQuery(name = "Bridge.findAll",
                 query = "SELECT b FROM Bridge b")
 })
-public class Bridge {
-
-    @Id
-    @Column(name = "BridgeID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @NotNull
-    private String name;
-
-    private String address;
-
-    private String city;
-
-    private String postCode;
-
-    private String country;
-
+public record Bridge(
+        @Id
+        @Column(name = "BridgeID")
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        Long id,
+        @NotNull
+        String name,
+        String address,
+        String city,
+        String postCode,
+        String country
+) {
+    public Bridge() {
+        this(null, null, null, null, null, null);
+    }
 }
