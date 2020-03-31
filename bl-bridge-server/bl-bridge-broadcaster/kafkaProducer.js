08e02d0ef239dc792a052559d0153b38cf8b4888
Joao Esperancinha <jofisaes@gmail.com>
2020-03-31 22:42:07 +0200

    First reader not random yet

--- bl-bridge-server/bl-bridge-broadcaster/kafkaProducer.js
+++ bl-bridge-server/bl-bridge-broadcaster/kafkaProducer.js
@@ -2,6 +2,7 @@ const kafka = require('kafka-node');
 const bp = require('body-parser');
 const config = require('./config');
 const topics = require('./topics');
+const {readdirSync, readFileSync} = require('fs');
 
 let produce = () => {
     try {
@@ -9,14 +10,16 @@ let produce = () => {
         const client = new kafka.KafkaClient({kafkaHost: config.kafka_server});
         const producer = new Producer(client);
         const kafka_topic = topics.temperature;
+        const files = readdirSync('points/');
+        console.log(files);
         console.log(kafka_topic);
-        let payloads = [
-            {
+        const payloads = files.map(
+            file => ({
                 topic: topics.temperature,
-                messages: "Hello Hello Are you there?"
-            }
-        ];
-
+                messages: readFileSync(`points/${file}`, 'utf8')
+            })
+        )
+        console.log(payloads);
         producer.on('ready', async function () {
             let push_status = producer.send(payloads, (err, data) => {
                 if (err) {
