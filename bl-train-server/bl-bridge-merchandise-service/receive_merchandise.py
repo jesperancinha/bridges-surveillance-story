63195d1afc04d3b8b5ae9a279813891bfb4e9013
Joao Esperancinha <jofisaes@gmail.com>
2020-04-08 13:44:57 +0200

    Enables the Shovel plugin

--- bl-train-server/bl-bridge-merchandise-service/receive_merchandise.py
+++ bl-train-server/bl-bridge-merchandise-service/receive_merchandise.py
@@ -4,20 +4,24 @@ import pika
 credentials = pika.PlainCredentials('test', 'test')
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost', port=5672,  credentials=credentials))
+    pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials, virtual_host='bl_merchandise_vh'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange='bl_merchandise_exchange', exchange_type='fanout', durable=True)
 
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
+# IF YOU WANT TO CREATE A NEW QUEUE ONLY!
+# result = channel.queue_declare(queue='bl_merchandise_queue', exclusive=True)
 
-channel.queue_bind(exchange='logs', queue=queue_name)
+queue_name = 'bl_merchandise_queue'
+
+channel.queue_bind(exchange='bl_merchandise_exchange', queue='bl_merchandise_queue')
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
+
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
+
 
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
--- bl-train-server/bl-bridge-merchandise-service/send_merchandise.py
+++ bl-train-server/bl-bridge-merchandise-service/send_merchandise.py
@@ -7,12 +7,10 @@ credentials = pika.PlainCredentials('test', 'test')
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost', port='5673', credentials=credentials, virtual_host='bl_merchandise_vh'))
 channel = connection.channel()
-channel.queue_declare("bl-merchandise-queue", durable=True)
-channel.exchange_declare(exchange='bl-merchandise-exchange', exchange_type='fanout', durable=True)
-
-
+channel.queue_declare("bl_merchandise_queue", durable=True)
+channel.exchange_declare(exchange='bl_merchandise_exchange', exchange_type='fanout', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='bl-merchandise-exchange', routing_key='', body=message)
+channel.basic_publish(exchange='bl_merchandise_exchange', routing_key='', body=message)
 print(" [x] Sent %r" % message)
 connection.close()
