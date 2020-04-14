236a71aa8ff58cd67baa6ff85bf27cc7a9e004d9
Joao Esperancinha <jofisaes@gmail.com>
2020-04-14 09:10:47 +0200

    Barcode is now a String

--- bl-central-server/bl-merchandise-control-service/src/main/java/org/jesperancinha/logistics/mcs/data/MerchandiseDto.java
+++ bl-central-server/bl-merchandise-control-service/src/main/java/org/jesperancinha/logistics/mcs/data/MerchandiseDto.java
@@ -1,20 +1,23 @@
 package org.jesperancinha.logistics.mcs.data;
 
 import lombok.Builder;
+import org.springframework.lang.NonNull;
 
-public record MerchandiseDto(Long id,
-    Long supplierId,
-    Long productId,
-    Long quantity,
-    Long volumePerUnit,
-    Long sourceLocationId,
-    Long destinationLocationId,
-    Long timeStamp,
-    Long barCode) {
+import javax.persistence.Id;
+
+public record MerchandiseDto(@Id @NonNull Long id,
+    @NonNull Long supplierId,
+    @NonNull Long productId,
+    @NonNull Long quantity,
+    @NonNull Long volumePerUnit,
+    @NonNull Long sourceLocationId,
+    @NonNull Long destinationLocationId,
+    @NonNull Long timeStamp,
+    @NonNull String barCode) {
     @Builder
-    public MerchandiseDto{
-
+    public MerchandiseDto {
     }
+
     public MerchandiseDto() {
         this(null, null, null, null, null, null, null, null, null);
     }
--- bl-central-server/bl-merchandise-control-service/src/main/java/org/jesperancinha/logistics/mcs/model/Merchandise.java
+++ bl-central-server/bl-merchandise-control-service/src/main/java/org/jesperancinha/logistics/mcs/model/Merchandise.java
@@ -1,6 +1,7 @@
 package org.jesperancinha.logistics.mcs.model;
 
 import lombok.Builder;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,24 +10,30 @@ import java.util.Objects;
 
 @Entity
 @Table
-public record Merchandise(@Id Long id,
-    Long supplierId,
-    Long productId,
-    Long quantity,
-    Long volumePerUnit,
-    Long sourceLocationId,
-    Long destinationLocationId,
-    Long timeStamp,
-    Long barCode) {
+public record Merchandise(@Id @NonNull Long id,
+    @NonNull Long supplierId,
+    @NonNull Long productId,
+    @NonNull Long quantity,
+    @NonNull Long volumePerUnit,
+    @NonNull Long sourceLocationId,
+    @NonNull Long destinationLocationId,
+    @NonNull Long timeStamp,
+    @NonNull String barCode) {
     @Builder
     public Merchandise {
+        if (Objects.isNull(id)) {
+            throw new IllegalArgumentException("A transaction must be supplied!");
+        }
         if (id > 0 && (Objects.isNull(supplierId) || supplierId < 0)) {
             throw new IllegalArgumentException("A supplier must be supplied!");
+        }
+        if (id > 0 && (Objects.isNull(productId) || productId < 0)) {
+            throw new IllegalArgumentException("A product must be supplied!");
         }
     }
 
     public Merchandise() {
-        this(-1L, null, null, null, null, null, null, null, null);
+        this(-1L, -1L, -1L, -1L, -1L, -1L, -1L, -1L, "");
     }
 
 }
