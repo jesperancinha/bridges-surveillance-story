5aa4d67be08e69b04801d7d126eefdadf45c60a5
Joao Esperancinha <jofisaes@gmail.com>
2020-04-16 15:41:42 +0200

    Federated sensor queue

--- bl-central-server/entrypoint.sh
+++ bl-central-server/entrypoint.sh
@@ -18,18 +18,23 @@ rabbitmqctl set_user_tags test administrator
 
 rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 
-rabbitmqctl add_vhost bl_merchandise_vh
+federate(){
+    rabbitmqctl add_vhost bl_$1_vh
 
-rabbitmqctl set_permissions -p bl_merchandise_vh test ".*" ".*" ".*"
+    rabbitmqctl set_permissions -p bl_$1_vh test ".*" ".*" ".*"
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare exchange name=bl_merchandise_exchange type=fanout
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare exchange name=bl_$1_exchange type=fanout
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare queue name=bl_merchandise_queue
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare queue name=bl_$1_queue
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare binding source=bl_merchandise_exchange destination=bl_merchandise_queue
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare binding source=bl_$1_exchange destination=bl_$1_queue
 
-rabbitmqctl set_parameter -p bl_merchandise_vh federation-upstream bl_merchandise_upstream '{"uri":"amqp://test:test@bl_train_server:5672/bl_merchandise_vh","expires":3600000}'
+    rabbitmqctl set_parameter -p bl_$1_vh federation-upstream bl_$1_upstream '{"uri":"amqp://test:test@bl_train_server:5672/bl_'$1'_vh","expires":3600000}'
 
-rabbitmqctl set_policy -p bl_merchandise_vh --apply-to all bl_merchandise_policy ".*merchandise.*" '{"federation-upstream-set":"all"}'
+    rabbitmqctl set_policy -p bl_$1_vh --apply-to all bl_$1_policy ".*$1.*" '{"federation-upstream-set":"all"}'
+}
+
+federate merchandise
+federate sensor
 
 tail -f /dev/null
--- bl-train-server/entrypoint.sh
+++ bl-train-server/entrypoint.sh
@@ -18,14 +18,19 @@ rabbitmqctl set_user_tags test administrator
 
 rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 
-rabbitmqctl add_vhost bl_merchandise_vh
+function sourceQueue(){
+    rabbitmqctl add_vhost bl_$1_vh
 
-rabbitmqctl set_permissions -p bl_merchandise_vh test ".*" ".*" ".*"
+    rabbitmqctl set_permissions -p bl_$1_vh test ".*" ".*" ".*"
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare exchange name=bl_merchandise_exchange type=fanout
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare exchange name=bl_$1_exchange type=fanout
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare queue name=bl_merchandise_queue
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare queue name=bl_$1_queue
 
-rabbitmqadmin -u test -p test -V bl_merchandise_vh declare binding source=bl_merchandise_exchange destination=bl_merchandise_queue
+    rabbitmqadmin -u test -p test -V bl_$1_vh declare binding source=bl_$1_exchange destination=bl_$1_queue
+}
+
+sourceQueue merchandise
+sourceQueue sensor
 
 tail -f /dev/null
