21f8526c2e0f1940ef209ec4ea9dbb7e155aec16
Joao Esperancinha <jofisaes@gmail.com>
2020-04-22 18:55:40 +0200

    Bridge Log

--- bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Bridge.java
+++ bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Bridge.java
@@ -1,21 +1,15 @@
 package org.jesperancinha.logistics.jpa.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "BRIDGE")
-@NamedQueries({ @NamedQuery(name = "Bridge.findAll",
-    query = "SELECT b FROM Bridge b") })
-public record Bridge(@Id @Column(name = "BridgeID") @GeneratedValue(strategy = GenerationType.AUTO)
-Long id,
+@Table(name = "bridge")
+public record Bridge(@Id @GeneratedValue(strategy = GenerationType.AUTO)Long id,
     @NotNull String name,
     String address,
     String city,
--- bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/BridgeLog.java
+++ bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/BridgeLog.java
@@ -1,10 +1,23 @@
 package org.jesperancinha.logistics.jpa.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
 @Table(name = "bridge_logs")
-public record BridgeLog(
-) {
+public record BridgeLog(@Id @GeneratedValue(strategy = GenerationType.AUTO)Long id,
+    @ManyToOne(cascade = ALL,
+        optional = false) @JoinColumn(name = "bridgeId",
+        nullable = false,
+        updatable = false)
+    Bridge bridge,
+    Long timestamp,
+    String checkInOut) {
 }
