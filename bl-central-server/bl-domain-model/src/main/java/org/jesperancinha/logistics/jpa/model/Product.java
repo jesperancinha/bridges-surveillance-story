ab0c6c041c652fce254302b6f62afe9f4da953b9
Joao Esperancinha <jofisaes@gmail.com>
2020-04-22 19:09:45 +0200

    Product model

--- bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Product.java
+++ bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Product.java
@@ -1,4 +1,25 @@
 package org.jesperancinha.logistics.jpa.model;
 
-public record Product() {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public record Product(@Id @GeneratedValue(strategy = GenerationType.AUTO)Long id,
+    String name,
+    String packaging,
+    String brand,
+    Long packageSize,
+    Long weightPerUnit,
+    String unitWeight,
+    String barcode,
+    Long correctionFactor,
+    Long width,
+    Long height,
+    Long depth,
+    String unitDimension
+) {
 }
