657fa55e9c3ca89d57e3e3351113bf4437f9d631
Joao Esperancinha <jofisaes@gmail.com>
2020-04-22 20:03:01 +0200

    Merchandise

--- bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Merchandise.java
+++ bl-central-server/bl-domain-model/src/main/java/org/jesperancinha/logistics/jpa/model/Merchandise.java
@@ -1,4 +1,14 @@
 package org.jesperancinha.logistics.jpa.model;
 
-public record Merchandise() {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "merchandise")
+public record Merchandise(@Id @GeneratedValue(strategy = GenerationType.AUTO)Long id,
+    Product product,
+    Long quantity) {
 }
