d600afd2019026770194f44931ea0e4b2f2a77e3
Joao Esperancinha <jofisaes@gmail.com>
2020-04-27 08:19:07 +0200

    Data Collector launcher

---
+++ bl-central-server/bl-sensor-data-collector/src/main/java/org/jesperancinha/logistics/sensor/collector/DataCollectorLauncher.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.logistics.sensor.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectorLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectorLauncher.class, args);
+    }
+
+}
