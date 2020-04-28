a5056dc966b11a62dc763f9f380e6d6ad82324ea
Joao Esperancinha <jofisaes@gmail.com>
2020-04-28 08:36:57 +0200

    📦 🚊 🚃 🚚 🚛 Sends merchandises functions

--- bl-train-server/bl-train-merchandise-service/send_merchandise.py
+++ bl-train-server/bl-train-merchandise-service/send_merchandise.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
+import json
 import sys
 
 import pika
 
-credentials = pika.PlainCredentials('test', 'test')
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost', port='5673', credentials=credentials, virtual_host='bl_merchandise_vh'))
-channel = connection.channel()
-channel.queue_declare("bl_merchandise_queue", durable=True)
-channel.exchange_declare(exchange='bl_merchandise_exchange', exchange_type='fanout', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "{\"id\":1, \"supplierId\":\"64985638639853\"}"
-channel.basic_publish(exchange='bl_merchandise_exchange', routing_key='', body=message)
-print(" [x] Sent %r" % message)
-connection.close()
+def send_merchandise(host, data):
+    data_json = json.dumps(data)
+    credentials = pika.PlainCredentials('test', 'test')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=host, port=5673, credentials=credentials, virtual_host='bl_train_01_merchandise_vh'))
+    channel = connection.channel()
+    channel.queue_declare("bl_train_01_merchandise_queue", durable=True)
+    channel.exchange_declare(exchange='bl_train_01_merchandise_exchange', exchange_type='fanout', durable=True)
+    message = ' '.join(sys.argv[1:]) or data_json
+    channel.basic_publish(exchange='bl_train_01_merchandise_exchange', routing_key='', body=message)
+    print(" [x] Sent %r" % message)
+    connection.close()
+
+
+if __name__ == '__main__':
+    send_merchandise('localhost',
+                     {"id": 1, "supplierId": "64985638639853"})
