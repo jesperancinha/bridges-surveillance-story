a0c39409c3665125912465058683d7fd64115514
Joao Esperancinha <jofisaes@gmail.com>
2020-05-03 19:45:46 +0200

    🕵️‍🕵️‍ Agatha Christie's

--- bl-demo-server/bl-core-service/launch_generate_people.py
+++ bl-demo-server/bl-core-service/launch_generate_people.py
@@ -5,9 +5,9 @@ import sys
 sys.path.insert(1, os.path.abspath('../bl-passenger-service'))
 sys.path.insert(2, os.path.abspath('bl-passenger-service'))
 
-from generate_people import generate_passengers
+from generate_people import generate_storyline_passengers
 
 if __name__ == '__main__':
-    passengers = generate_passengers(300)
+    passengers = generate_storyline_passengers(300)
     with open('./../../bl-simulation-data/passengers/passengers.json', 'w') as file:
         file.write(json.dumps(passengers))
