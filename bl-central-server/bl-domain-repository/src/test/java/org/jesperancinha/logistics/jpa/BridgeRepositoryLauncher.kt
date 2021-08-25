d3b40dc16e79cbda6a0fb1e7150e4318d89f979e
Joao Esperancinha <jofisaes@gmail.com>
2021-08-25 19:40:30 +0200

    🌿 BridgeRepositoryTest

---
+++ bl-central-server/bl-domain-repository/src/test/java/org/jesperancinha/logistics/jpa/BridgeRepositoryLauncher.kt
@@ -0,0 +1,21 @@
+package org.jesperancinha.logistics.jpa
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+/**
+ * Created by jofisaes on 25/08/2021
+ */
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableAutoConfiguration
+class BridgeRepositoryLauncher {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(BridgeRepositoryLauncher::class.java)
+        }
+    }
+}
