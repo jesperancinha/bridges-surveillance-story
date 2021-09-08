461cd5573d246f2744200e6e2997af16e073e021
Joao Esperancinha <jofisaes@gmail.com>
2021-09-08 18:41:06 +0100

    🐘 Removes embedded stuff

--- bl-central-server/bl-merchandise-data-collector/src/test/java/org/jesperancinha/logistics/mcs/TrainMerchandiseReceiverTest.java
+++ bl-central-server/bl-merchandise-data-collector/src/test/java/org/jesperancinha/logistics/mcs/TrainMerchandiseReceiverTest.java
@@ -1,37 +1,39 @@
 package org.jesperancinha.logistics.mcs;
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
+@Testcontainers
 public class TrainMerchandiseReceiverTest {
 
-    private static EmbeddedPostgres embeddedPostgres;
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres")
+                    .withUsername("postgres")
+                    .withPassword("admin");
 
     @BeforeAll
     public static void initialise() throws Exception {
-        if (embeddedPostgres == null) {
-            embeddedPostgres = EmbeddedPostgres.builder()
-                .setPort(5433)
-                .start();
-
-            try (Connection conn = embeddedPostgres.getPostgresDatabase()
-                .getConnection()) {
-                Statement statement = conn.createStatement();
-                statement.execute("CREATE DATABASE bllogistics");
-            }
+        try (Connection conn = DriverManager.getConnection(postgreSQLContainer.getJdbcUrl(), "postgres","admin");
+        ) {
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE DATABASE bllogistics");
         }
     }
+
 
     @Test
     public void receiveMessage() {
