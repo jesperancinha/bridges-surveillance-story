e6e2107d9ee9b8d4161947fc6393ab6b76df20ba
Joao Esperancinha <jofisaes@gmail.com>
2022-09-04 20:41:46 +0200

    Creates image for spark reading processes

---
+++ bl-central-server/bl-central-readings/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+java --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED -jar bl-meters-readings-service.jar demo &
+
+java --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED -jar bl-passengers-reading.jar demo &
+
+tail -f /dev/null
