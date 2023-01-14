880161b1c1c6beb6841e8acb39b955a2ef3a20be
Joao Esperancinha <jofisaes@gmail.com>
2023-01-14 21:12:34 +0100

    Dtos with the JsonProperty set

--- bl-central-server/bl-web-app/src/main/kotlin/org/jesperancinha/logistics/web/dto/Dto.kt
+++ bl-central-server/bl-web-app/src/main/kotlin/org/jesperancinha/logistics/web/dto/Dto.kt
@@ -1,12 +1,18 @@
 package org.jesperancinha.logistics.web.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
 data class BridgeDto(
+    @JsonProperty("name")
     val name: String,
+    @JsonProperty("address")
     val address: String? = null,
+    @JsonProperty("city")
     val city: String? = null,
+    @JsonProperty("postCode")
     val postCode: String? = null,
+    @JsonProperty("country")
     val country: String? = null
 )
 
@@ -15,7 +21,9 @@ data class BridgeDto(
  * It will accumulate all conflicts found in reference to the first element in the list
  */
 data class BridgeOpeningConflictDto(
+    @JsonProperty("message")
     val message: String? = null,
+    @JsonProperty("relatedOpeningTimes")
     val relatedOpeningTimes: MutableList<BridgeOpeningTimeDto>
 )
 
@@ -23,44 +31,67 @@ data class BridgeOpeningConflictDto(
  * This object serves to determine a time slot to open and close a bridge
  */
 data class BridgeOpeningTimeDto(
+    @JsonProperty("bridge")
     val bridge: BridgeDto,
+    @JsonProperty("openingTime")
     val openingTime: LocalDateTime,
+    @JsonProperty("closingTime")
     val closingTime: LocalDateTime
 )
 
 data class CarriageFullDto(
+    @JsonProperty("carriageId")
     val carriageId: Long,
+    @JsonProperty("packageId")
     val packageId: Long,
+    @JsonProperty("people")
     val people: Long,
+    @JsonProperty("products")
     val products: List<ProductDto>
 )
 
 data class ContainerFullDto(
+    @JsonProperty("containerId")
     val containerId: Long,
+    @JsonProperty("packageId")
     val packageId: Long,
+    @JsonProperty("products")
     val products: List<ProductDto>
 )
 
 data class FreightDto(
+    @JsonProperty("id")
     val id: Long,
+    @JsonProperty("name")
     val name: String,
+    @JsonProperty("type")
     val type: String,
+    @JsonProperty("supplierId")
     val supplierId: Long,
+    @JsonProperty("vendorId")
     val vendorId: Long,
+    @JsonProperty("composition")
     val composition: List<ContainerFullDto>
 )
 
-@JvmRecord
 data class ProductDto(
+    @JsonProperty("productId")
     val productId: Long,
+    @JsonProperty("quantity")
     val quantity: Long
 )
 
 data class TrainDto(
+    @JsonProperty("id")
     val id: Long,
+    @JsonProperty("name")
     val name: String,
+    @JsonProperty("type")
     val type: String,
+    @JsonProperty("supplierId")
     val supplierId: Long,
+    @JsonProperty("vendorId")
     val vendorId: Long,
+    @JsonProperty("composition")
     val composition: List<CarriageFullDto>
 )
