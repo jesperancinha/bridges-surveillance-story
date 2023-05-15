316c20ae830d8ac12f0827b31988142045343ea1
Joao Esperancinha <jofisaes@gmail.com>
2023-05-15 19:52:20 +0200

    Makefile improvements and code cleanup

--- bl-central-server/bl-domain-repository/src/test/kotlin/org/jesperancinha/logistics/jpa/BridgeRepositoryLauncher.kt
+++ bl-central-server/bl-domain-repository/src/test/kotlin/org/jesperancinha/logistics/jpa/BridgeRepositoryLauncher.kt
@@ -1,7 +1,6 @@
 package org.jesperancinha.logistics.jpa
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
